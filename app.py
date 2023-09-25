b9650183a3a211f8babd3d32ba028e3c00612550
Szymon Sztukowski <szy.szt@gmail.com>
2023-09-25 15:08:30 +0200

    Authentication features

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def transcribe_audio_file():
 ########
 
 # User authentication:
-with open('./.streamlit/config.yaml') as file:
+with open('./config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 authenticator = stauth.Authenticate(
